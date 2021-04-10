4f820e390e8450c15c7f0278ca896432ea613622
Khaled Skairek <khaled.skairek@gmail.com>
2021-04-10 09:58:38 +0200

    Enable run log

--- starter_file/entry.py
+++ starter_file/entry.py
@@ -1,13 +1,12 @@
 import argparse
-# from azureml.core.run import Run
+from azureml.core.run import Run
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-print(tf.__version__)
 from tensorflow import keras
 
-# run = Run.get_context()
+run = Run.get_context()
 
 # Read data from csv
 X = pd.read_csv(r"./../../data/heart_failure_clinical_records_dataset.csv")
@@ -15,6 +14,7 @@ Y = X.pop("DEATH_EVENT")
 
 # Split data into train and test sets.
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
 
 def main():
     # Add arguments to script
@@ -29,29 +29,22 @@ def main():
 
     args = parser.parse_args()
 
-    # run.log("learning_rate:", np.float(args.learning_rate))
-    # run.log("epochs:", np.float(args.epochs))
-    # run.log("neurons:", np.float(args.neurons))
-
-    # When
-    # # model = SVC(C=args.C, kernel='poly', degree=args.degree)
-    # model = SVC(C=0.4, kernel='poly', degree=4)
-    # clf = make_pipeline(StandardScaler(), model)
-    # clf.fit(x_train, y_train)
+    run.log("learning_rate:", np.float(args.learning_rate))
+    run.log("epochs:", np.float(args.epochs))
+    run.log("neurons:", np.float(args.neurons))
 
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(12,)),
         keras.layers.BatchNormalization(),
-
         keras.layers.Dense(units=args.neurons, activation=tf.nn.relu, kernel_regularizer='l2'),
         keras.layers.BatchNormalization(),
-
         keras.layers.Dense(units=1, activation=tf.nn.sigmoid)
     ])
 
+    # Create the model architecture
     opt = keras.optimizers.Adam(learning_rate=args.learning_rate)
-    model.compile(optimizer=opt, # Generate new guess about the input/output relation
-                  loss='binary_crossentropy', # Calculate how good the guess was
+    model.compile(optimizer=opt,  # Generate new guess about the input/output relation
+                  loss='binary_crossentropy',  # Calculate how good the guess was
                   metrics=['accuracy'])
 
     model.fit(X_train, Y_train, epochs=args.epochs)
@@ -60,8 +53,10 @@ def main():
     test_loss, test_acc = model.evaluate(X_test, Y_test)
     print('Test accuracy:', test_acc)
 
-    model.save('my_model'+'_LR_'+str(args.learning_rate)+'_epochs_'+str(args.epochs) + '_units_'+str(args.neurons))
+    model.save(
+        'my_model' + '_LR_' + str(args.learning_rate) + '_epochs_' + str(args.epochs) + '_units_' + str(args.neurons))
 
-    # run.log("Accuracy", np.float(test_acc))
+    run.log("accuracy", np.float(test_acc))
+
 
 main()
