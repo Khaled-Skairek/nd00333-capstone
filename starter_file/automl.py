0d3686ab4c3408f816346a783f335057c72c7f20
Khaled Skairek <khaled.skairek@gmail.com>
2021-04-11 13:11:22 +0000

    Add files via upload

--- starter_file/automl.py
+++ starter_file/automl.py
@@ -140,7 +140,7 @@ joblib.dump(fitted_model, "best_model_auto_ml.model")
 # 
 # TODO: In the cell below, register the model, create an inference config and deploy the model as a web service.
 
-# In[16]:
+# In[35]:
 
 
 from azureml.core import Model
@@ -149,7 +149,6 @@ from azureml.core.resource_configuration import ResourceConfiguration
 model = Model.register(workspace=ws,
                        model_name='my-autoML-model',                # Name of the registered model in your workspace.
                        model_path='./best_model_auto_ml.model',     # Local file to upload and register as a model.
-                       model_framework=Model.Framework.SCIKITLEARN, # Framework used to create the model.
                        resource_configuration=ResourceConfiguration(cpu=1, memory_in_gb=0.5),
                        description='VotingEnsemble') # ,
                        # tags={'area': 'diabetes', 'type': 'classification'})
@@ -158,14 +157,24 @@ print('Name:', model.name)
 print('Version:', model.version)
 
 
-# In[23]:
+# In[31]:
+
+
+print(ws.environments['AzureML-VowpalWabbit-8.8.0'])
+
+
+# In[36]:
 
 
 from azureml.core.model import InferenceConfig
 from azureml.core.webservice import AciWebservice
 from azureml.core import Environment
 
-environment = Environment('my-environment')
+# environment = Environment('my-environment')
+# environment = ws.environments['AzureML-VowpalWabbit-8.8.0']
+
+environment = Environment.get(workspace=ws, name="AzureML-AutoML")
+
 
 service_name = 'heart-failure-prediction'
 
@@ -189,7 +198,61 @@ service.wait_for_deployment(show_output=True)
 # In[ ]:
 
 
+import requests
+import json
 
+# URL for the web service, should be similar to:
+# 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
+scoring_uri = ''
+# If the service is authenticated, set the key or token
+key = ''
+
+# Two sets of data to score, so we get two results back
+data = {"data":
+        [
+          {
+            "age": 17,
+            "anaemia": 1,
+            "creatinine_phosphokinase": 600,
+            "diabetes": 1,
+            "ejection_fraction": 30,
+            "high_blood_pressure": 0,
+            "platelets": 263000,
+            "serum_creatinine": 1.2,
+            "serum_sodium": 130,
+            "sex": 1,
+            "smoking": 0,
+            "time": 15,
+          },
+          {
+            "age": 35,
+            "anaemia": 0,
+            "creatinine_phosphokinase": 500,
+            "diabetes": 0,
+            "ejection_fraction": 30,
+            "high_blood_pressure": 0,
+            "platelets": 280000,
+            "serum_creatinine": 2.1,
+            "serum_sodium": 150,
+            "sex": 0,
+            "smoking": 0,
+            "time": 10,
+          },
+      ]
+    }
+# Convert to JSON string
+input_data = json.dumps(data)
+with open("data.json", "w") as _f:
+    _f.write(input_data)
+
+# Set the content type
+headers = {'Content-Type': 'application/json'}
+# If authentication is enabled, set the authorization header
+headers['Authorization'] = f'Bearer {key}'
+
+# Make the request and display the response
+resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp.json())
 
 
 # TODO: In the cell below, print the logs of the web service and delete the service
@@ -197,5 +260,8 @@ service.wait_for_deployment(show_output=True)
 # In[ ]:
 
 
+service.get_logs(num_lines=100)
+# service.delete()
 
+# AmlCompute.delete(cpu_cluster)
 
